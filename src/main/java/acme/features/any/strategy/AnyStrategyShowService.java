61216a11336e8bb2ac181c7b1bb9241303ed433a
dansalhid <danielsaladohidalgo@gmail.com>
2026-03-09 13:27:32 +0100

    Arreglar bug ShowService

--- src/main/java/acme/features/any/strategy/AnyStrategyShowService.java
+++ src/main/java/acme/features/any/strategy/AnyStrategyShowService.java
@@ -32,9 +32,8 @@ public class AnyStrategyShowService extends AbstractService<Any, Strategy> {
 
 	@Override
 	public void unbind() {
-		super.unbindObject(this.strategy, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo");
-
-		super.unbindObject(this.strategy, "fundraiser.userAccount.identity.name", "fundraiser.userAccount.identity.surname", "fundraiser.userAccount.identity.email", "fundraiser.bank", "fundraiser.statement");
+		super.unbindObject(this.strategy, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "fundraiser.userAccount.identity.name", "fundraiser.userAccount.identity.surname", "fundraiser.userAccount.identity.email",
+			"fundraiser.bank", "fundraiser.statement");
 
 		super.unbindGlobal("monthsActive", this.strategy.getMonthsActive());
 		super.unbindGlobal("expectedPercentage", this.strategy.getExpectedPercentage());
