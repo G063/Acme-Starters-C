dc438727e196eaa0b58f09b004f7382af78847ef
dansalhid <danielsaladohidalgo@gmail.com>
2026-03-09 12:46:27 +0100

    Requisito 2 Estudiante 3

---
+++ src/main/java/acme/features/any/strategy/AnyStrategyShowService.java
@@ -0,0 +1,42 @@
+
+package acme.features.any.strategy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.principals.Any;
+import acme.client.services.AbstractService;
+import acme.entities.strategy.Strategy;
+
+@Service
+public class AnyStrategyShowService extends AbstractService<Any, Strategy> {
+
+	@Autowired
+	protected AnyStrategyRepository	repository;
+
+	protected Strategy				strategy;
+
+
+	@Override
+	public void load() {
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		this.strategy = this.repository.findStrategyById(id);
+	}
+
+	@Override
+	public void authorise() {
+		super.setAuthorised(this.strategy != null && !this.strategy.getDraftMode());
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObject(this.strategy, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo");
+
+		super.unbindObject(this.strategy, "fundraiser.userAccount.identity.name", "fundraiser.userAccount.identity.surname", "fundraiser.userAccount.identity.email", "fundraiser.bank", "fundraiser.statement");
+
+		super.unbindGlobal("monthsActive", this.strategy.getMonthsActive());
+		super.unbindGlobal("expectedPercentage", this.strategy.getExpectedPercentage());
+	}
+}
