d8db167c319564feb133f6608bae70835997fd4f
Carlos Vázquez <70294882+CarlosVP05@users.noreply.github.com>
2026-03-20 18:09:20 +0100

    Arreglo textos del draftMode

--- src/main/java/acme/features/inventor/invention/InventorInventionListService.java
+++ src/main/java/acme/features/inventor/invention/InventorInventionListService.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.client.components.models.Tuple;
+import acme.client.helpers.MessageHelper;
 import acme.client.services.AbstractService;
 import acme.entities.invention.Invention;
 import acme.realms.Inventor;
@@ -38,6 +40,20 @@ public class InventorInventionListService extends AbstractService<Inventor, Inve
 
 	@Override
 	public void unbind() {
-		super.unbindObjects(this.inventions, "ticker", "name", "cost", "draftMode");
+		for (Invention s : this.inventions) {
+			Tuple tuple;
+
+			tuple = this.unbindObject(s, "ticker", "name", "cost");
+			tuple.put("draftMode", this.getDraftModeLabel(s.getDraftMode()));
+		}
+
+	}
+
+	private String getDraftModeLabel(final Boolean draftMode) {
+		String key;
+
+		key = Boolean.TRUE.equals(draftMode) ? "inventor.invention.form.value.draft" : "inventor.invention.form.value.published";
+
+		return MessageHelper.getMessage(key);
 	}
 }
--- src/main/resources/WEB-INF/views/any/part/form.jsp
+++ src/main/resources/WEB-INF/views/any/part/form.jsp
@@ -8,10 +8,4 @@
 	<acme:form-money code="any.part.label.cost" path="cost" readonly="true"/>
 	<acme:form-textarea code="any.part.label.description" path="description" readonly="true"/>
 
-	<hr/>
-
-	<h3><acme:print code="any.part.label.invention-info"/></h3>
-
-	<acme:form-textbox code="any.part.label.invention.ticker" path="invention.ticker" readonly="true"/>
-
 </acme:form>
